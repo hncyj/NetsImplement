10d88876def3af25a8b8ad214603f773691db005
chenyinjie <chenyinjie666@foxmail.com>
2025-05-16 10:02:04 +0800

    2025/05/16 <feat> implement dataloader.

--- demo.py
+++ demo.py
@@ -1,8 +1,81 @@
-########################
-#                      #
-#     Hello, World!    #
-#                      #
-#######################
+import random
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import torch
+from  torchvision import transforms
+from torch.utils.data import Dataset, DataLoader
+
+class FashionMNISTDataset(Dataset):
+    def __init__(self, path, transform=None):
+        super().__init__()
+        self.path = path
+        self.imgs = pd.read_csv(path).iloc[:, 1:].values.astype(np.uint8)
+        self.labels = pd.read_csv(path).iloc[:, 0].values
+        self.transform = transform
+    
+    def __len__(self):
+        if len(self.imgs) != len(self.labels):
+            raise ValueError(f"images len: {len(self.imgs)} not equals to labels len: {len(self.labels)}")
+        return len(self.imgs)
+    
+    def __getitem__(self, idx):
+        img = self.imgs[idx].reshape(28, 28, -1)
+        label = int(self.labels[idx])
+        
+        if self.transform is not None:
+            img = self.transform(img)
+        else:
+            img = torch.tensor(img / 255., dtype=torch.float)
+            
+        label = torch.tensor(label, dtype=torch.long)
+        
+        return img, label
+        
 
 if __name__ == "__main__":
-    print("Hello, World!")
+    labels_map = {
+        0: "T-Shirt",
+        1: "Trouser",
+        2: "Pullover",
+        3: "Dress",
+        4: "Coat",
+        5: "Sandal",
+        6: "Shirt",
+        7: "Sneaker",
+        8: "Bag",
+        9: "Ankle Boot",
+    }
+    
+    batch_size = 256
+    lr = 1e-4
+    epochs = 20
+    
+    transform = transforms.Compose(
+        [transforms.ToTensor()]
+    )
+    
+    train_datasets = FashionMNISTDataset("./datasets/FashionMNIST/fashion-mnist_train.csv", transform=transform)
+    train_loader = DataLoader(train_datasets, batch_size=batch_size, shuffle=True, drop_last=False)
+    
+    test_datasets = FashionMNISTDataset("./datasets/FashionMNIST/fashion-mnist_train.csv", transform=transform)
+    test_loader = DataLoader(test_datasets, batch_size=batch_size, shuffle=True, drop_last=False)
+    
+    ## 检查数据集是否正确加载
+    # train_features, train_labels = next(iter(train_loader))
+    
+    # indices = random.sample(range(len(train_features)), 9)
+    # fig, axes = plt.subplots(3, 3, figsize=(10, 10))
+    # axes = axes.flatten()
+    
+    # for i, idx in enumerate(indices):
+    #     img = train_features[idx].squeeze()
+    #     label = train_labels[idx].item()
+    #     axes[i].set_title(f"{labels_map[label]}")
+    #     axes[i].imshow(img, cmap="gray")
+    #     axes[i].axis('off')
+    
+    # plt.tight_layout()
+    # plt.show()
